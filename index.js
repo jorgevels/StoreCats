d9045a05e990b74be5d5bbb713570b98cac7ef7a
jorgevels <jorgevelasquez006@hotmail.com>
2020-01-07 11:35:24 -0500

    Subiendo proyecto Primer commit

---
+++ index.js
@@ -0,0 +1,38 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/Desarrollo-web-profesional/sw.js")
+    .then(function() {
+      console.log("Service Worker Registrado");
+    });
+}
+
+// Código para manejar la solicitud de instalación en el escritorio
+
+let deferredPrompt;
+const addBtn = document.querySelector(".btn-warning");
+addBtn.style.display = "none";
+
+window.addEventListener("beforeinstallprompt", e => {
+  // Evita que Chrome 67 y versiones anteriores muestren automáticamente el aviso
+  e.preventDefault();
+  // Guarda el evento para que pueda activarse más tarde.
+  deferredPrompt = e;
+  // Actualizar la interfaz de usuario para notificar al usuario que puede agregar a la pantalla de inicio
+  addBtn.style.display = "block";
+
+  addBtn.addEventListener("click", e => {
+    // Oculta nuestra interfaz de usuario que muestra nuestro botón A2HS.
+    addBtn.style.display = "none";
+    // Mostrar el aviso
+    deferredPrompt.prompt();
+    // Espere a que el usuario responda a la pregunta
+    deferredPrompt.userChoice.then(choiceResult => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("El usuario acepto la instalacion");
+      } else {
+        console.log("El usuario cancelo la instalacion ");
+      }
+      deferredPrompt = null;
+    });
+  });
+});
---
+++ sw.js
@@ -0,0 +1,52 @@
+const VERSION = "v1";
+
+// Se llama cuando el navegador instale el sw
+self.addEventListener("install", event => {
+  // El precache recibe una lista de recursos que querramoes que mantenfa en cache
+  event.waitUntil(precache());
+});
+
+// Cada vez que ocurra  una pericion quiero que el sw haga algo
+self.addEventListener("fetch", event => {
+  // Extrar la peticion
+  const request = event.request;
+  // get solo traer los get
+  // si el metodo  del request no es un get no vamos hacer nada
+  // significa que el request va seguir en internet
+  if (request.method !== "GET") {
+    return;
+  }
+
+  // buscar en cache
+  event.respondWith(cachedResponse(request));
+
+  // actualizar el cache
+  event.waitUntil(updateCache(request));
+});
+
+//Nos da una instancia de un cache que le va pertenecer a la version uno y regresa una promesa
+async function precache() {
+  const cache = await caches.open(VERSION);
+
+  // Añadimos una lista de todos nuestros recursos del proyecto
+  return cache.addAll([
+    // Es muy importante ya que las paginas tambien las solicitamos como / debemos capturar esta request
+  ]);
+}
+
+// Buscando recursos en el cache
+async function cachedResponse(request) {
+  const cache = await caches.open(VERSION);
+  // Le preguntamos al cache, ya tienes una copia que le corresponde al request
+  // si no tiene nada va el request en internet
+  const response = await cache.match(request);
+  // Retorna el cache encontrado o va al request de internet
+  return response || fetch(request);
+}
+
+// Buscando una copia actualizada de todos los archivos de la aplicacion
+async function updateCache(request) {
+  const cache = await caches.open(VERSION);
+  const response = await fetch(request);
+  return cache.put(request, response);
+}
